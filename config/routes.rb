0ff6069687e2ebcabc2e86260e356a8b4b9c9fc7
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-12 14:49:21 -0700

    ui mockups

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'ui#index'
+
   get 'ui(/:action)', controller: 'ui'
+
+  get '/login', to: "sessions#new"
+  post '/logout', to: "sessions#destroy"
+  
+  resources :users, except: [:destroy]
+
+  resources :businesses, only: [:show, :index, :create, :new] do
+    resources :reviews, only: [:create, :update]
+  end
 end
