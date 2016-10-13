96a7bf7b8c264c40b959c4b4659e8f9cbba5c1f5
Michael Mentele <Michaelrmentele@gmail.com>
2016-10-12 18:51:37 -0700

    functionality implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Congratz! You're registered. You can now leave reviews."
+    else
 
+    end
+
+    redirect_to login_path
   end
 
   def show
@@ -8,6 +15,12 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 describe UsersController do
   describe "GET show" do 
-    it "sets @reviews"
+    it "sets @reviews" 
   end
 
   describe "POST create" do
     context "with valid inputs" do
-      it "creates a new user"
-      it "sets flash log in notice"
+      before { post :create, user: Fabricate.attributes_for(:user) }
+      it "creates a new user" do 
+        expect(User.count).to eq(1)
+      end
+      it "sets flash log in notice" do 
+        expect(flash[:notice]).to_not be_nil
+      end
       it "sets the created user as the logged in user"
       it "redirects to businesses path"
     end
@@ -21,6 +26,9 @@ describe UsersController do
   end
 
   describe "GET new" do 
-    it "sets @user"
+    it "sets @user" do
+      get :new
+      expect(assigns(:user)).to be_instance_of(User)
+    end
   end
 end
