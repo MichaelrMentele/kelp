b9a7c776227da4c4b65986a548ee32c064581d76
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-03 15:33:14 -0700

    changed fabricator to have valid price

--- spec/fabricators/coupon_fabricator.rb
+++ spec/fabricators/coupon_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:coupon) do 
   for_sale { false }
   discount_percent { 10 }
-  price { Faker::Number.number(1) }
+  price { Faker::Number.number(3) }
   description { Faker::Lorem.paragraph(1) }
 end
